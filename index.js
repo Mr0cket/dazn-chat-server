7bc24395636e8c5eaf2c6509c66b88f8a7726ebf
Milo Silva <milo.silva234@gmail.com>
2020-12-31 20:29:11 +0100

    initialise socket.io & serve index.html from '/'

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(__dirname + "/chatUi/index.html"); // this is an iframe
 });
 
-app.listen(PORT, () => console.log(`listening on :${PORT}`));
+// WebSocket connections
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(PORT, () => console.log(`listening on :${PORT}`));
